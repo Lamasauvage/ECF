e7f10c3e01671e487e07acabc43b5c89fbc2b88a
Guillaume ADAM <adam.guillaume78@gmail.com>
2022-12-22 14:54:46 +0100

    Projet structure

---
+++ Site Web/front/src/pages/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Head -->
+    <?php include_once 'C:\Users\gadam\Documents\XAMPP\htdocs\STUDI\ECF\Site Web\front\src\templates\head.php'; ?>
+  </head>
+
+  <body>
+    <!-- Navbar -->
+    <?php include_once 'C:\Users\gadam\Documents\XAMPP\htdocs\STUDI\ECF\Site Web\front\src\templates\navbar.php'; ?>
+
+    
+  </body>
+</html>
+
---
+++ Site Web/front/src/templates/navbar.php
@@ -0,0 +1,3 @@
+<div>
+  <h1>Le Quai Antique</h1>
+</div>

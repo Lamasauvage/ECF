2b58f7d1fdea15173156cb5c65fc2899487fec64
Guillaume ADAM <adam.guillaume78@gmail.com>
2023-02-01 22:16:15 +0100

    carte.php menu fix

--- SiteWeb/front/src/pages/carte.php
+++ SiteWeb/front/src/pages/carte.php
@@ -3,6 +3,8 @@
 <?php include "../../../css/style.css"; ?>
 </style>
 
+
+
 <div class="filter">
   <a href="#" class="btn" data-filter="all">Tout</a>
   <a href="#" class="btn" data-filter="entree">Entrée</a>
@@ -19,20 +21,30 @@
     <th>Prix</th>
     <th>Catégorie</th>
   </tr>
+
   <?php
   include_once '../../../includes/csv.inc.php';
-  $sql = "SELECT * FROM dishes ORDER BY title";
+  $sql = "SELECT * FROM dishes ORDER BY CASE WHEN categorie = 'entree' THEN 1 WHEN categorie = 'plat' THEN 2 WHEN categorie = 'dessert' THEN 3 WHEN categorie = 'vin' THEN 4 END
+  ";
   $result = mysqli_query($conn, $sql);
 
   if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-      ?>
-   <tr class="menu-item <?php echo $row['categorie']; ?>">
-    <td class="menu-item-title"><?php echo $row['title']; ?></td>
-    <td><?php echo $row['description']; ?></td>
-    <td class="menu-item-price"><?php echo $row['price']; ?>€</td>
-    <td><?php echo $row['categorie']; ?></td>
-  </tr>
+
+    $categories = array(
+    'entree' => 'Entrée',
+    'plat' => 'Plat',
+    'dessert' => 'Dessert',
+    'vin' => 'Vin');
+  ?>
+
+    <tr class="menu-item <?php echo $row['categorie']; ?>">
+      <td class="menu-item-title"><?php echo $row['title']; ?></td>
+      <td><?php echo $row['description']; ?></td>
+      <td class="menu-item-price"><?php echo $row['price']; ?>€</td>
+      <td><?php echo $categories[$row['categorie']]; ?></td>
+    </tr>
+    
   <?php
   }
 }
